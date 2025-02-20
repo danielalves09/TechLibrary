100f69ad8d8a53e4a9114dd0e88b5b695e528483
Daniel Alves <daniel.alvesmoreira1@gmail.com>
2025-02-19 22:35:56 -0300

    :sparkles: feat: Validações da Request

--- TechLibrary.Api/UseCases/Users/Register/RegisteruserValidator.cs
+++ TechLibrary.Api/UseCases/Users/Register/RegisteruserValidator.cs
@@ -1,4 +1,5 @@
 ﻿using FluentValidation;
+using TechLibrary.Communication.Requests;
 
 namespace TechLibrary.Api.UseCases.Users.Register
 {
@@ -6,7 +7,15 @@ namespace TechLibrary.Api.UseCases.Users.Register
     {
         public RegisteruserValidator()
         {
-            
+            RuleFor(request => request.Name).NotEmpty().WithMessage("O nome é obrigatorio.");
+            RuleFor(request => request.Email).EmailAddress().WithMessage("O email é obrigatorio.");
+            RuleFor(request => request.Password).NotEmpty().WithMessage("A senha é obrigatoria.");
+            When(request => string.IsNullOrEmpty(request.Password) == false, () =>
+            {
+                RuleFor(request => request.Password).MinimumLength(6).WithMessage("A senha deve ter no minimo 6 caracteres.");
+            });
+
+
         }
 
     }
