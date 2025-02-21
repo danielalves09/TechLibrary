4d5623b08a54277bfabc38be257ac15814bde8d7
Daniel Alves <daniel.alvesmoreira1@gmail.com>
2025-02-21 00:44:20 -0300

    feat: Inserindo Paginação

--- TechLibrary.Api/UseCases/Books/Filter/FilterBookUseCase.cs
+++ TechLibrary.Api/UseCases/Books/Filter/FilterBookUseCase.cs
@@ -6,15 +6,28 @@ namespace TechLibrary.Api.UseCases.Books.Filter
 {
     public class FilterBookUseCase
     {
+        private const int PAGE_SIZE = 10;
 
         public ResponseBooksJson Execute(RequestFilterBooksJson request)
         {
             var dbContext = new TechLibraryDbContext();
 
-            var books = dbContext.Books.ToList();
+            var books = dbContext
+                .Books
+                .OrderBy(book => book.Title).ThenBy(book => book.Author)
+                .Skip((request.PageNumber - 1) * PAGE_SIZE)
+                .Take(PAGE_SIZE)
+                .ToList();
+
+            var totalCount = dbContext.Books.Count();
 
             return new ResponseBooksJson
             {
+                Pagination = new ResponsePaginationJson()
+                {
+                    PageNumber = request.PageNumber,
+                    TotalCount = totalCount
+                },
                 Books = books.Select(book => new ResponseBookJson()
                 {
                     id = book.Id,
