655b7cd05b05edb26665d68b33d2405598922fb2
Daniel Alves <daniel.alvesmoreira1@gmail.com>
2025-02-21 01:10:37 -0300

    feat: inserido  Filtro pelo Título do livro

--- TechLibrary.Api/UseCases/Books/Filter/FilterBookUseCase.cs
+++ TechLibrary.Api/UseCases/Books/Filter/FilterBookUseCase.cs
@@ -1,4 +1,5 @@
-﻿using TechLibrary.Api.Infraestructure.DataAcess;
+﻿using TechLibrary.Api.Domain.Entities;
+using TechLibrary.Api.Infraestructure.DataAcess;
 using TechLibrary.Communication.Requests;
 using TechLibrary.Communication.Responses;
 
@@ -12,30 +13,44 @@ namespace TechLibrary.Api.UseCases.Books.Filter
         {
             var dbContext = new TechLibraryDbContext();
 
-            var books = dbContext
-                .Books
+           
+
+            var query = dbContext.Books.AsQueryable();
+
+
+            if (string.IsNullOrEmpty(request.Title) == false)
+            {
+                query = query.Where(book => book.Title.Contains(request.Title));
+                
+            }
+
+            var books = query       
                 .OrderBy(book => book.Title).ThenBy(book => book.Author)
                 .Skip((request.PageNumber - 1) * PAGE_SIZE)
                 .Take(PAGE_SIZE)
                 .ToList();
 
-            var totalCount = dbContext.Books.Count();
+            var totalCount = 0;
+            if (string.IsNullOrWhiteSpace(request.Title))
+             totalCount = dbContext.Books.Count();
+            else
+                totalCount = dbContext.Books.Count(book => book.Title.Contains(request.Title));
 
             return new ResponseBooksJson
-            {
-                Pagination = new ResponsePaginationJson()
                 {
-                    PageNumber = request.PageNumber,
-                    TotalCount = totalCount
-                },
-                Books = books.Select(book => new ResponseBookJson()
-                {
-                    id = book.Id,
-                    title = book.Title,
-                    author = book.Author,
-                }).ToList()
+                    Pagination = new ResponsePaginationJson()
+                    {
+                        PageNumber = request.PageNumber,
+                        TotalCount = totalCount
+                    },
+                    Books = books.Select(book => new ResponseBookJson()
+                    {
+                        id = book.Id,
+                        title = book.Title,
+                        author = book.Author,
+                    }).ToList()
 
-            };
+                };
 
         }
 
